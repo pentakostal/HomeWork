6ce6551900255624fd23fe3276fc5d52095a8a4d
Pavels Skokovs <dstars72@gmail.com>
2022-11-01 18:34:53 +0200

    Exercise 3 end

--- 2022_11_03/exercise_3.php
+++ 2022_11_03/exercise_3.php
@@ -1,1 +1,10 @@
 <?php
+$number = readline("Enter number: ");
+if ($number >= 0) {
+    $number_split = array_map('intval', str_split($number));
+    $counter = count($number_split);
+    echo "Number of digits in number is " . $counter . PHP_EOL;
+} else {
+    echo "Number must be positive." . PHP_EOL;
+}
+
