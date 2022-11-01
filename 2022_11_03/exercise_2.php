8ddf3cdb21d1e2a51308558846db5c7aac1cb290
Pavels Skokovs <dstars72@gmail.com>
2022-11-01 18:26:48 +0200

    Exercise 2 end

--- 2022_11_03/exercise_2.php
+++ 2022_11_03/exercise_2.php
@@ -1,1 +1,9 @@
 <?php
+$number = readline("Enter the number: ");
+if ($number > 0) {
+    echo "Number is positive" . PHP_EOL;
+} elseif ($number < 0) {
+    echo "Number is negative" . PHP_EOL;
+} else {
+    echo "Number is zero" . PHP_EOL;
+}
