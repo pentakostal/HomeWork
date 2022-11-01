a38fe8969b6ca97c9ca01421a4e1b158eb071dc8
Pavels Skokovs <dstars72@gmail.com>
2022-11-01 18:48:39 +0200

    Exercise 4 end

--- 2022_11_03/exercise_4.php
+++ 2022_11_03/exercise_4.php
@@ -1,1 +1,27 @@
 <?php
+$day = readline("Enter number of the day: ");
+switch ($day) {
+    case 1:
+        echo "Monday" . PHP_EOL;
+        break;
+    case 2:
+        echo "Tuesday" . PHP_EOL;
+        break;
+    case 3:
+        echo "Wednsaday" . PHP_EOL;
+        break;
+    case 4:
+        echo "Thursday" . PHP_EOL;
+        break;
+    case 5:
+        echo "Friday" . PHP_EOL;
+        break;
+    case 6:
+        echo "Saturday" . PHP_EOL;
+        break;
+    case 7:
+        echo "Sunday" . PHP_EOL;
+        break;
+    default:
+        echo "Invalid number" . PHP_EOL;
+}
