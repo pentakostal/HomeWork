9b881b12565cb74ed8ba8e097655e35dc931943c
Pavels Skokovs <dstars72@gmail.com>
2022-11-06 19:04:01 +0200

    Exercise 8 complete

--- 2022_11_06/loops/exercise_8.php
+++ 2022_11_06/loops/exercise_8.php
@@ -1,1 +1,24 @@
 <?php
+class numberSquare {
+    public $min, $max;
+    function __construct($min, $max){
+        $this->min=$min;
+        $this->max=$max;
+    }
+    function square(){
+        $myArrayOfNumbers=range($this->min, $this->max);
+        echo implode("", $myArrayOfNumbers).PHP_EOL;
+        for($i=1;$i<count($myArrayOfNumbers);$i++){
+            foreach ($myArrayOfNumbers as $key=>$item) {
+                if($item==$this->max){
+                    $myArrayOfNumbers[$key] = $this->min;
+                } else {
+                    $myArrayOfNumbers[$key] = $item + 1;
+                }
+            }
+            echo implode(" ", $myArrayOfNumbers).PHP_EOL;
+        }
+    }
+}
+$play=new numberSquare((int)readline("Min? "), (int)readline("Max? "));
+$play->square();
