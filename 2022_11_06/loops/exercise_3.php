81f326ecdf6a8e3c49fdfce338785eba654e50db
Pavels Skokovs <dstars72@gmail.com>
2022-11-06 16:04:52 +0200

    exercise 3 complete

--- 2022_11_06/loops/exercise_3.php
+++ 2022_11_06/loops/exercise_3.php
@@ -1,1 +1,14 @@
 <?php
+$wordFirst = (string) readline("Enter first word: ");
+$wordSecond = (string) readline("Enter second word: ");
+$i=0;
+while (TRUE) {
+    $result = $wordFirst.str_repeat(".", $i).$wordSecond;
+    if (strlen($result)<30) {
+        $i++;
+    } else {
+        echo $result;
+        break;
+    }
+
+}
