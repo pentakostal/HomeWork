bccffee04bcdc67e8c524e93a32893469d344786
Pavels Skokovs <dstars72@gmail.com>
2022-11-07 00:00:17 +0200

    exercise 2 complete

--- 2022_11_07/classes-and-objects/exercise_2.php
+++ 2022_11_07/classes-and-objects/exercise_2.php
@@ -1,1 +1,22 @@
 <?php
+declare(strict_types=1);
+class Point {
+    public $x, $y;
+    function __construct(int $x, int $y)
+    {
+        $this->x=$x;
+        $this->y=$y;
+    }
+    function swapPoints($coordinateOne, $coordinateTwo)
+    {
+        $coordinateOne->x ^=$coordinateTwo->x ^=$coordinateOne->x ^=$coordinateTwo->x;
+        $coordinateOne->y ^=$coordinateTwo->y ^=$coordinateOne->y ^=$coordinateTwo->y;
+    }
+}
+$coordinateOne=new Point(5, 2);
+$coordinateTwo=new Point(-3, 6);
+echo "(" . $coordinateOne->x . ", " . $coordinateOne->y . ")".PHP_EOL;
+echo "(" . $coordinateTwo->x . ", " . $coordinateTwo->y . ")".PHP_EOL;
+$coordinateOne->swapPoints($coordinateOne, $coordinateTwo);
+echo "(" . $coordinateOne->x . ", " . $coordinateOne->y . ")".PHP_EOL;
+echo "(" . $coordinateTwo->x . ", " . $coordinateTwo->y . ")".PHP_EOL;
