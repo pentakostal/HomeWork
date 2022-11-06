8abb67b93ffb683d6a02f4ce2037162a2ab6beee
Pavels Skokovs <dstars72@gmail.com>
2022-11-06 20:20:01 +0200

    Exercise 6 complete

--- 2022_11_06/loops/exercise_6.php
+++ 2022_11_06/loops/exercise_6.php
@@ -1,3 +1,16 @@
 <?php
-$str="************************************************";
-echo strlen($str);
+class AsciiFigure {
+    public $number;
+    function __construct($number){
+        $this->number=$number;
+    }
+    function buildPyramid() {
+        echo str_repeat("/",$this->number-1).str_repeat("\\",$this->number-1).PHP_EOL;
+        for($i=1;$i<$this->number;$i++){
+            echo str_repeat("/",$this->number-$i-1).str_repeat("*",$i).
+                str_repeat("*",$i).str_repeat("\\",$this->number-$i-1).PHP_EOL;
+        }
+    }
+}
+$build=new AsciiFigure(readline("Enter number-> "));
+$build->buildPyramid();
