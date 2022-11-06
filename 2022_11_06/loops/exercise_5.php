0fa60dfcac1317f18ad9b3c61126545a0b99530f
Pavels Skokovs <dstars72@gmail.com>
2022-11-06 18:14:24 +0200

    exercis 5 complete

--- 2022_11_06/loops/exercise_5.php
+++ 2022_11_06/loops/exercise_5.php
@@ -1,1 +1,29 @@
 <?php
+class pigLet {
+    private $score;
+    function __construct($score) {
+        (int) $this->score=$score;
+    }
+
+    function game() {
+        while (true) {
+            (string) $choice = readline("Roll dice (y/n): ");
+            if ($choice == "y") {
+                $value = rand(1, 6);
+                if ($value == 1) {
+                    echo "You rolled 1".PHP_EOL;
+                    echo "You got 0 points".PHP_EOL;
+                    break;
+                } else {
+                    echo "You rolled $value".PHP_EOL;
+                    $this->score += $value;
+                }
+            } else {
+                echo "You got $this->score points".PHP_EOL;
+                break;
+            }
+        }
+    }
+}
+$game = new pigLet(0);
+echo $game->game();
