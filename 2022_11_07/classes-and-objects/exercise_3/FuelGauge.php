5d7b532bf74631ec05bb71f9fcfbc779dd4e9092
Pavels Skokovs <dstars72@gmail.com>
2022-11-07 18:06:36 +0200

    exercise 3 FuelGauge class complite

--- 2022_11_07/classes-and-objects/exercise_3/FuelGauge.php
+++ 2022_11_07/classes-and-objects/exercise_3/FuelGauge.php
@@ -1,1 +1,37 @@
 <?php
+class FuelGauge
+{
+    private int $amount;
+
+    public function __construct($amount)
+    {
+        $this->amount=$amount;
+    }
+
+    public function getAmount(): int
+    {
+        return $this->amount;
+    }
+
+    public function fill(int $fillAmount = 1): void
+    {
+        $maxAmount = 70;
+        $newAmount = $this->amount + $maxAmount;
+        if ($newAmount > $maxAmount) {
+            $newAmount = $maxAmount;
+        }
+        $this->amount = $newAmount;
+    }
+
+    public function use(int $useAmount = 1): void
+    {
+        $minAmount = 0;
+        $newAmount = $this->amount - $useAmount;
+
+        if ($this->amount > $minAmount) {
+            $newAmount = $this->amount - $useAmount;
+        }
+
+        $this->amount = $newAmount;
+    }
+}
