d313b76c4c265c45b6a4054ddeaf165a3d008a83
Pavels Skokovs <dstars72@gmail.com>
2022-11-07 19:10:41 +0200

    Exercise 3 main logic complete

--- 2022_11_07/classes-and-objects/exercise_3/exercise_3.php
+++ 2022_11_07/classes-and-objects/exercise_3/exercise_3.php
@@ -1,1 +1,28 @@
 <?php
+require_once 'FuelGauge.php';
+require_once 'Odometer.php';
+
+$fuelGauge = new FuelGauge(5);
+$odometer = new Odometer(999989);
+
+//var_dump($fuelGauge);
+//var_dump($odometer);
+$index = 0;
+while ($fuelGauge->getAmount() > 0) {
+    //Driving
+    echo "We drove 1 km".PHP_EOL;
+    $odometer->increaseMileage();
+    echo $odometer->getMileage() .PHP_EOL;
+    $index++;
+    //Fuel consumtion
+    if($index % 10 === 0) {
+        $fuelGauge->use(1);
+        echo "Remove 1 liter.".PHP_EOL;
+    }
+    //Check fuel tank
+    if($fuelGauge->getAmount() == 0){
+        echo "You are out of fuel".PHP_EOL;
+        break;
+    }
+    sleep(1);
+}
