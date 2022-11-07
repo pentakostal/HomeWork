58e27ca6b870fc26f3ad7171e08bf3de9ab06585
Pavels Skokovs <dstars72@gmail.com>
2022-11-07 18:20:22 +0200

    Exercise 3 Odometer complete

--- 2022_11_07/classes-and-objects/exercise_3/Odometer.php
+++ 2022_11_07/classes-and-objects/exercise_3/Odometer.php
@@ -1,1 +1,25 @@
 <?php
+class Odometer
+{
+    private int $mileage;
+
+    public function __construct(int $mileage)
+    {
+        $this->mileage=$mileage;
+    }
+
+    public function getMileage(): int
+    {
+        return $this->mileage;
+    }
+
+    public function increaseMileage(int $amount = 1): void
+    {
+        $maxMileage = 999999;
+        $newMileage = $this->mileage + $amount;
+        if ($newMileage > $maxMileage) {
+            $newMileage = 0;
+        }
+        $this->mileage = $newMileage;
+    }
+}
