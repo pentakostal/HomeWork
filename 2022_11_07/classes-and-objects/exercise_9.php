893858a1bfc6b4b0d943da92092c7d53f18a5669
Pavels Skokovs <dstars72@gmail.com>
2022-11-08 23:27:17 +0200

    Exercise 9 complite

--- 2022_11_07/classes-and-objects/exercise_9.php
+++ 2022_11_07/classes-and-objects/exercise_9.php
@@ -1,1 +1,26 @@
 <?php
+class BankAccount
+{
+    private string $name;
+    private float $balance;
+
+    public function __construct(string $name, float $balance) {
+        $this->name = $name;
+        $this->balance = $balance;
+    }
+
+    public function showUserNameAndBalance(): string
+    {
+        if ($this->balance > 0) {
+            $display = $this->name . " , $" . $this->balance;
+        } elseif ($this->balance < 0) {
+            $display = $this->name . " , -$" . abs($this->balance);
+        }
+        return $display;
+    }
+}
+
+$den = new BankAccount("Den", 434.6);
+$carl = new BankAccount("Carl", -17.3);
+echo $den->showUserNameAndBalance() . PHP_EOL;
+echo $carl->showUserNameAndBalance() . PHP_EOL;
