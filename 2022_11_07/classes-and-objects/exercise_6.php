f2be45592c49f3e5216aa306004552f2d655f4a9
Pavels Skokovs <dstars72@gmail.com>
2022-11-08 16:38:50 +0200

    Exercise 6 complete

--- 2022_11_07/classes-and-objects/exercise_6.php
+++ 2022_11_07/classes-and-objects/exercise_6.php
@@ -1,1 +1,24 @@
 <?php
+class CustomerTaste
+{
+    private int $customerNumber;
+
+    public function __construct($customerNumber)
+    {
+        $this->customerNumber=$customerNumber;
+    }
+
+    public function getMoreEnergy():string
+    {
+        return round($this->customerNumber * 0.14) . " people purchased one ore more energy drinks.";
+    }
+    
+    public function getCitrusDrinks(): string
+    {
+        return round(round($this->customerNumber * 0.14) * 0.64) . " people prefer citrus flavored drinks.";
+    }
+}
+
+$customers = new CustomerTaste(12467);
+echo $customers->getMoreEnergy() . PHP_EOL;
+echo $customers->getCitrusDrinks() . PHP_EOL;
