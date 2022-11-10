cffef6986f126d1de3fb5271b7e57ef643362888
Pavels Skokovs <dstars72@gmail.com>
2022-11-10 14:51:07 +0200

    Exercise 10 main logic complite

--- 2022_11_07/classes-and-objects/exercise_10/test.php
+++ 2022_11_07/classes-and-objects/exercise_10/test.php
@@ -1,1 +1,19 @@
 <?php
+//For debug and checking
+require_once 'Video.php';
+require_once 'VideoStore.php';
+
+$program = new VideoStore();
+$program->addVideo("The Matrix");
+$program->addVideo("Reservoir Dogs");
+$program->addVideo("Godfather II");
+$program->addVideo("Stars Wars Episode IV: A New Hope");
+$program->addVideo("Pulp Fiction");
+
+
+//echo $program->listVideo() . PHP_EOL;
+$program->signRating("The Matrix", 9.5);
+$program->checkOutStore("The Matrix");
+$program->returneToStore("The Matrix");
+echo $program->listVideo() .  PHP_EOL;
+
